60ed6356b207db8675a91f0a000eaf88f18e229e
Ketan Devraj <k10@Ketans-MacBook-Air.local>
2026-04-11 01:50:20 +0530

    Final deployment fix

--- app.py
+++ app.py
@@ -79,5 +79,8 @@ def clear_logs():
     return redirect(url_for("index"))
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
